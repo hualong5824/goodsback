055859dd307c94cb6750429f830f8755f40be008
hl <hualong5824@163.com>
2019-07-03 19:13:42 +0800

    登录页样式完成

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,13 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-
+import Vue from 'vue';
+import Router from 'vue-router';
 Vue.use(Router)
-
+import login from '@/views/login.vue'
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+		name:'login',
+    path:'/login',
+		component:login
     }
   ]
-})
+});
---
+++ src/views/login.vue
@@ -0,0 +1,35 @@
+<template>
+	<div class="login-box">
+		<el-form label-width="80px" class="login-form" label-position="top">
+			<h2>用户登录</h2>
+			<el-form-item label="账号">
+				<el-input></el-input>
+			</el-form-item>
+			<el-form-item label="密码">
+				<el-input></el-input>
+			</el-form-item>
+		 <el-button type="primary" class="login-box-btn">登录</el-button>
+		</el-form>
+		
+	</div>
+</template>
+<script>
+</script>
+<style>
+	.login-form{
+		width: 500px;
+		background: #fff;
+		padding: 30px;
+		border-radius: 5px;
+	}
+	.login-box-btn{
+		width: 100%;
+	}
+	.login-box {
+		height: 100%;
+		background: #314052;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+</style>
